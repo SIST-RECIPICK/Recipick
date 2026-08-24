48ad814d04a1cff39aba855c9db7ad683d14adb0
jeunju528_sudo <228353885+jeunju528-sudo@users.noreply.github.com>
2026-08-24 16:31:41 +0900

    initial

---
+++ src/main/java/com/sist/web/RecipickApplication.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipickApplication.class, args);
+	}
+
+}
