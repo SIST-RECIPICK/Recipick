92b1c6958d8918f56658e7a828840c64d1a5c5b0
kcw858 <cksdn858@naver.com>
2026-09-02 17:40:41 +0900

    [김찬우>]securityConfig 기본 설정

--- src/main/java/com/sist/web/config/SecurityConfig.java
+++ src/main/java/com/sist/web/config/SecurityConfig.java
@@ -1,8 +1,33 @@
 package com.sist.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		http
+			.csrf(csrf -> csrf.disable()) //위조 방지
+			.sessionManagement(session -> 
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
+			.formLogin(form -> form.disable())
+			.authorizeHttpRequests(auth -> auth
+				.anyRequest().permitAll()
+			);
+			
+		return http.build();
+	}
+	
 	
 }
