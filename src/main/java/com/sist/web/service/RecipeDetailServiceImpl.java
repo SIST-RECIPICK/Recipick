20f0e026797230d30cb1ad695fd0090ae87e5807
kcw858 <cksdn858@naver.com>
2026-09-07 01:52:54 +0900

    [김찬우] 좋아요,북마크 구현

--- src/main/java/com/sist/web/service/RecipeDetailServiceImpl.java
+++ src/main/java/com/sist/web/service/RecipeDetailServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.sist.web.mapper.RecipeDetailMapper;
 import com.sist.web.vo.IngredientUnitVO;
+import com.sist.web.vo.RecipeLikeVO;
 import com.sist.web.vo.RecipeManualVO;
 import com.sist.web.vo.RecipeVO;
 import com.sist.web.vo.ShopLinkVO;
@@ -43,5 +44,41 @@ public class RecipeDetailServiceImpl implements RecipeDetailService{
 		}
 	
 		return unitList;
+	}
+
+	@Override
+	public int recipeDetailLikeExist(int rcp_seq,int user_id) {
+	
+		return mapper.recipeDetailLikeExist(rcp_seq,user_id);
+	}
+
+	@Override
+	public void recipeDetailLikeInsert(int rcp_seq,int user_id) {
+		
+		mapper.recipeDetailLikeInsert(rcp_seq,user_id);
+	}
+
+	@Override
+	public void recipeDetailLikeDelete(int rcp_seq,int user_id) {
+		
+		mapper.recipeDetailLikeDelete(rcp_seq,user_id);
+	}
+
+	@Override
+	public int recipeDetailBookmarkExist(int recipe_id, int user_id) {
+		
+		return mapper.recipeDetailBookmarkExist(recipe_id, user_id);
+	}
+
+	@Override
+	public void recipeDetailBookmarkInsert(int recipe_id, int user_id) {
+		
+		mapper.recipeDetailBookmarkInsert(recipe_id, user_id);
+	}
+
+	@Override
+	public void recipeDetailBookmarkDelete(int recipe_id, int user_id) {
+		
+		mapper.recipeDetailBookmarkDelete(recipe_id, user_id);
 	}
 }
