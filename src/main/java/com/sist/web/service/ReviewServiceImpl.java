f64bfc5ce144ef8d4088a981aa4e95abd0b86caa
fhzmx2-blip <fhzmx2@gmail.com>
2026-09-07 16:45:11 +0900

    Feature/review (#21)
    * fhzmx2: work in progress
    
    * fhzmx2: mapper(work in progress)
    
    * fhzmx2: work in progress
    
    * push test
    
    * fhzmx2: test page success
    
    * fhzmx2: test page success
    
    * gitignore test
    
    * test gitignore
    
    * test
    
    * test
    
    * fhzmx2: review list data update
    
    * fhzmx2: mapper update
    
    * fhzmx2: making detail page
    
    * fhzmx2: review list, detail, insert, update, delete, reply
    
    * fhzmx2: making search
    
    * fhzmx2: review search

---
+++ src/main/java/com/sist/web/service/ReviewServiceImpl.java
@@ -0,0 +1,93 @@
+package com.sist.web.service;
+
+import java.util.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sist.web.mapper.ReviewMapper;
+import com.sist.web.vo.Review_BoardVO;
+import com.sist.web.vo.Review_Board_ReplyVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewServiceImpl implements ReviewService {
+	private final ReviewMapper rMapper;
+	private final int ROW_SIZE = 12;
+
+	@Override
+    public List<Review_BoardVO> ReviewBoardListData(int page, String keyword, String type) {
+        int start = (page - 1) * ROW_SIZE;
+        return rMapper.ReviewBoardListData(start, keyword, type);
+    }
+
+    @Override
+    public int reviewBoardTotalpage(String keyword, String type) {
+        return rMapper.reviewBoardTotalpage(keyword, type);
+    }
+
+    @Override
+    public int[] pages(int page, String keyword, String type) {
+        int totalpage = rMapper.reviewBoardTotalpage(keyword, type);
+        final int BLOCK = 10;
+
+        int startpage = ((page - 1) / BLOCK * BLOCK) + 1;
+        int endpage = ((page - 1) / BLOCK * BLOCK) + BLOCK;
+        if (endpage > totalpage) {
+            endpage = totalpage;
+        }
+
+        int[] pages = { page, totalpage, startpage, endpage };
+        return pages;
+    }
+
+	@Override
+	public Review_BoardVO boardDetailData(int id) {
+	    rMapper.boardHitUpdate(id);
+	    return rMapper.boardDetailData(id);
+	}
+	@Override
+	public List<Review_BoardVO> writerOtherReviews(Map<String, Object> map) {
+		return rMapper.writerOtherReviews(map);
+	}
+
+	@Override
+	public List<Review_BoardVO> recipeOtherReviews(Map<String, Object> map) {
+		return rMapper.recipeOtherReviews(map);
+	}
+
+	@Override
+	public List<Review_Board_ReplyVO> boardReplyList(int reviewBoardId) {
+		return rMapper.boardReplyList(reviewBoardId);
+	}
+	
+	@Override
+	public void reviewInsert(Review_BoardVO vo) {
+	    rMapper.reviewInsert(vo);
+	}
+	
+	@Override
+	public void reviewUpdate(Review_BoardVO vo) {
+	    rMapper.reviewUpdate(vo);
+	}
+	
+	@Override
+	@Transactional
+	public void reviewDelete(int id) {
+	    rMapper.reviewReplyAllDelete(id);
+	    rMapper.reviewDelete(id);
+	}
+	
+	@Override
+	public void reviewReplyInsert(Review_Board_ReplyVO vo) {
+	    rMapper.reviewReplyInsert(vo);
+	}
+
+	@Override
+	public void reviewReplyDelete(int id) {
+	    rMapper.reviewReplyDelete(id);
+	}
+
+}
