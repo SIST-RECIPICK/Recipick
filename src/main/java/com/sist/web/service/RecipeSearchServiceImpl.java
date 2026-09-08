5a665af2c40a05dba0663c8165ca4dd78c717e23
atg8915 <ttajja69@gmail.com>
2026-09-08 17:48:36 +0900

    atg8915 기능 : 미리보기 데이터 로직 구현          ..

--- src/main/java/com/sist/web/service/RecipeSearchServiceImpl.java
+++ src/main/java/com/sist/web/service/RecipeSearchServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.sist.web.mapper.RecipeSearchMapper;
+import com.sist.web.vo.RecipePreviewVO;
 import com.sist.web.vo.RecipeSearchVO;
 
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,11 @@ public class RecipeSearchServiceImpl implements RecipeSearchService{
 	public List<RecipeSearchVO> selectRecipeSearch(String keyword, int user_id) {
 		// TODO Auto-generated method stub
 		return rsMapper.selectRecipeSearch(keyword,user_id);
+	}
+	@Override
+	public RecipePreviewVO selectRecipePreview(int rcp_seq) {
+		// TODO Auto-generated method stub
+		return rsMapper.selectRecipePreview(rcp_seq);
 	}
 
 }
