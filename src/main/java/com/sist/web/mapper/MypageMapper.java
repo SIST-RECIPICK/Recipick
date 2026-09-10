da7b35579974d96a5e98d09e7a885eca608ed726
fhzmx2-blip <fhzmx2@gmail.com>
2026-09-10 16:05:14 +0900

    fhzmx2: merge error

--- src/main/java/com/sist/web/mapper/MypageMapper.java
+++ src/main/java/com/sist/web/mapper/MypageMapper.java
@@ -1,10 +1,12 @@
 package com.sist.web.mapper;
 
-import java.util.Map;
+import java.util.*;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import com.sist.web.vo.Review_BoardVO;
+import com.sist.web.vo.Review_Board_ReplyVO;
 import com.sist.web.vo.UsersVO;
 
 @Mapper
@@ -14,4 +16,10 @@ public interface MypageMapper {
     public UsersVO mypageSideProfile(@Param("userId") int userId);
 
     public Map<String, Object> mypageMainCount(@Param("userId") int userId);
+    
+    public List<Review_BoardVO> myReviewList(@Param("userId") int userId, @Param("start") int start);    
+    public List<Review_Board_ReplyVO> myReviewReplyList(@Param("userId") int userId, @Param("start") int start);
+    public void deleteMyReviews(@Param("userId") int userId, @Param("idList") List<Integer> idList);
+    public void deleteMyReviewReplies(@Param("userId") int userId, @Param("idList") List<Integer> idList);
+    
 }
