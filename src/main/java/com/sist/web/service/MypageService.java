d23c3105352263684df9727bef723c31becade61
fhzmx2-blip <fhzmx2@gmail.com>
2026-09-16 13:57:44 +0900

    fhzmx2: mypage main, review, reply
    * fhzmx2: work in progress
    
    * fhzmx2: mapper(work in progress)
    
    * fhzmx2: work in progress
    
    * push test
    
    * fhzmx2: test page success
    
    * fhzmx2: test page success
    
    * gitignore test
    
    * test gitignore
    
    * test
    
    * test
    
    * fhzmx2: review list data update
    
    * fhzmx2: mapper update
    
    * fhzmx2: making detail page
    
    * fhzmx2: review list, detail, insert, update, delete, reply
    
    * fhzmx2: making search
    
    * fhzmx2: review search
    
    * fhzmx2: mypage branch
    
    * fhzmx2: main page
    
    * fhzmx2: work in progress
    
    * fhzmx2: merge error
    
    * fhzmx2: mypage review, reply, delete
    
    * fhzmx2: mypage review, reply, delete
    
    * fhzmx2: test
    
    * fhzmx2: work in progress

---
+++ src/main/java/com/sist/web/service/MypageService.java
@@ -0,0 +1,19 @@
+package com.sist.web.service;
+
+import java.util.*;
+import com.sist.web.vo.*;
+
+public interface MypageService {
+
+	public UsersVO mypageProfile(int id);
+
+    public Map<String, Object> mypageMainCount(int id);
+
+    public List<Review_BoardVO> myReviewList(int id, int page);
+    public int myReviewTotalPage(int id);
+    public List<Review_Board_ReplyVO> myReviewReplyList(int id, int page);
+    public int myReviewReplyTotalPage(int id);
+
+    public void deleteMyReview(int userId, int reviewId);
+    public void deleteMyReviewReplies(int id, List<Integer> deleteReplyList);
+}
