3bc8a231634b9440afd115298c3eaaabf55118fe
fhzmx2-blip <fhzmx2@gmail.com>
2026-09-17 17:46:29 +0900

    fhzmx2: social link

--- src/main/java/com/sist/web/service/SocialAuthService.java
+++ src/main/java/com/sist/web/service/SocialAuthService.java
@@ -2,7 +2,9 @@ package com.sist.web.service;
 
 import com.sist.web.dto.GoogleLoginRequest;
 import com.sist.web.dto.LoginResponse;
+import com.sist.web.dto.SocialLinkRequest;
 
 public interface SocialAuthService {
 	LoginResponse googleLogin(GoogleLoginRequest request);
+	LoginResponse linkAndLogin(SocialLinkRequest request);
 }
