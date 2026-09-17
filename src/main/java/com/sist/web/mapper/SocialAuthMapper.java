62dbf1300d8f1e72bb3e96e932904bb2f499180f
fhzmx2-blip <fhzmx2@gmail.com>
2026-09-17 15:33:41 +0900

    fhzmx2: social login

---
+++ src/main/java/com/sist/web/mapper/SocialAuthMapper.java
@@ -0,0 +1,19 @@
+package com.sist.web.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.sist.web.vo.SocialAccountVO;
+import com.sist.web.vo.UsersVO;
+
+@Mapper
+@Repository
+public interface SocialAuthMapper {
+	SocialAccountVO findSocialAccount(@Param("provider") String provider, @Param("provider_id") String provider_id);
+	SocialAccountVO findSocialAccountByUserId(@Param("users_id") int users_id);
+	int insertSocialAccount(SocialAccountVO socialAccount);
+	UsersVO findUserByEmail(@Param("email") String email);
+	UsersVO findUserById(@Param("id") int id);
+	int insertUser(UsersVO user);
+}
